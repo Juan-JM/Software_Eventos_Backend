84e9a3e9877be25d9475923cb10214d9af18485a
Juan <juan_noe10hotmail.com>
2025-04-27 00:20:07 -0400

    railway_v3

--- planit_one_backend/settings.py
+++ planit_one_backend/settings.py
@@ -15,7 +15,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-$k0m&rnyf9wua_=@37aaouf3m9w5s)zu)*joz89glas47!_!&i'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 ALLOWED_HOSTS = ['localhost', '127.0.0.1','softwareeventosbackend-production.up.railway.app']
 
 
