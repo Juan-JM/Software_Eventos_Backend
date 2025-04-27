82c718be97a91982891d3d498e59fd5b31e49bc6
Juan <juan_noe10hotmail.com>
2025-04-27 00:22:33 -0400

    railway_v3

--- planit_one_backend/settings.py
+++ planit_one_backend/settings.py
@@ -165,4 +165,4 @@ STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 
 STATICFILES_STORAGE="whitenoise.storage.CompressedManifestStaticFilesStorage"
 
-CSRF_TRUSTED_ORIGINS=['http://*','https://softwareeventosbackend-production.up.railway.app/']
+CSRF_TRUSTED_ORIGINS=['http://*','https://softwareeventosbackend-production.up.railway.app']
