480e6c11d57049c62179fafaca59652a884f9d37
Juan <juan_noe10hotmail.com>
2025-04-28 23:17:27 -0400

    ServidorF

--- events/views.py
+++ events/views.py
@@ -2,8 +2,9 @@ from rest_framework import viewsets
 from .models import Event
 from .serializers import EventSerializer
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from audit.mixins import AuditModelMixin
 
-class EventViewSet(viewsets.ModelViewSet):
+class EventViewSet(AuditModelMixin,viewsets.ModelViewSet):
     queryset = Event.objects.all()
     serializer_class = EventSerializer
     permission_classes = [IsAuthenticatedOrReadOnly]
