fbbe46890d45ed217fc9a97684d62b58f40e8371
Rodrigo <rodrigoriverogonzales@gmail.com>
2025-05-27 09:01:54 -0400

    b

--- planit_one_backend/settings.py
+++ planit_one_backend/settings.py
@@ -90,20 +90,20 @@ WSGI_APPLICATION = 'planit_one_backend.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 #Servidor Railway
-#DATABASES ={
-#    'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
-#}
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('DB_NAME'),
-        'USER': os.getenv('DB_USER'),
-        'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': os.getenv('DB_HOST'),
-        'PORT': os.getenv('DB_PORT'),
-    }
+DATABASES ={
+    'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
 }
+
+#DATABASES = {
+#    'default': {
+#       'ENGINE': 'django.db.backends.postgresql',
+#       'NAME': os.getenv('DB_NAME'),
+#       'USER': os.getenv('DB_USER'),
+#       'PASSWORD': os.getenv('DB_PASSWORD'),
+#       'HOST': os.getenv('DB_HOST'),
+#       'PORT': os.getenv('DB_PORT'),
+#   }
+#}
 
 # DATABASES = {
 #     'default': {
